21b7cadcc14ac6e4450436600f3c05864133487d
abed allateef qaisi <abed_q@hotmail.com>
2019-10-11 18:04:50 +0200

    change break points

--- azir-core/azir-theme/breakPoints.js
+++ azir-core/azir-theme/breakPoints.js
@@ -1,9 +1,13 @@
 const BREAKPOINTS = {
-  xl: 1920,
-  lg: 1280,
-  md: 960,
-  sm: 600,
+  xl: 1280,
+  lg: 980,
+  md: 736,
+  sm: 480,
   xs: 0
 };
-
+// xl: 1920,
+// lg: 1280,
+// md: 960,
+// sm: 600,
+// xs: 0
 export default BREAKPOINTS;
